9736703ad56dd9d74e9e8fed4a1a57c971ffb751
Priyashi Gupta <priyanshigupta0303@gmail.com>
2025-01-31 00:40:51 +0530

    myPortfolio

--- src/components/ContactMe/ContactForm.jsx
+++ src/components/ContactMe/ContactForm.jsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import './ContactForm.css';
-
-// const ContactForm = () => {
-//   return (
-//     <div className='contact-form-content'>
-//         <form>
-//         <div className='name-container'>
-//             <input type = "text" name = "firstname"
-//             placeholder='First Name'></input>
-//             <input type = "text" name = "lastname"
-//             placeholder='Last Name'></input>
-//         </div>
-//         <input type = "text" name = "email" placeholder='Email'/>
-//             <textarea type = "text" name = "message" placeholder = "Message" rows = {3}/>
-//             <button>SEND</button>
-//         </form>
-//     </div>
-//   )
-// }
-
-// export default ContactForm
-
 import React, { useState } from 'react';
 import './ContactForm.css';
 import emailjs from 'emailjs-com'; // Import EmailJS
